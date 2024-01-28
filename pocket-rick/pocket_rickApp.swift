2d0894d258ffcc854ac796a4a2b539272a2eff7e
Du Yuhan <yuhan.du@gmail.com>
2024-01-28 16:50:32 +0800

    Initial Commit

---
+++ pocket-rick/pocket_rickApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocket_rickApp.swift
+//  pocket-rick
+//
+//  Created by Yuhan Du on 28/1/24.
+//
+
+import SwiftUI
+
+@main
+struct pocket_rickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
